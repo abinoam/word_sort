4c21019c7c0523a8eadcb7d05c7e604f9ee79b7f
Abinoam Jr <abinoam@gmail.com>
2013-08-14 15:16:06 -0300

    The best way to really 'break' the iteration

--- words_sort.rb
+++ words_sort.rb
@@ -11,10 +11,9 @@ prev_word = nil
 
 while more < words.length
     0.upto(words.length-1) do |word_ix|
-      if lower_ix < (words.length - 1)
-        if next_word != :reset
-          prev_word = next_word
-          next_word = words[word_ix]
+        prev_word = next_word
+        next_word = words[word_ix]
+        if prev_word != :reset
             if prev_word and next_word and next_word != prev_word
               if prev_word > next_word
                 words[lower_ix] = next_word
@@ -24,16 +23,12 @@ while more < words.length
                 #Here, I skip over the rest of the each loop and do the
                 #while loop again
                 #so I don't over write any of the old values
-                lower_ix += words.length
-
+                break
               end
             end
-        else
-          next_word = words[word_ix]
         end
           lower_ix += 1
           higher_ix += 1
-      end
     end
     lower_ix = -1
     higher_ix = 0
