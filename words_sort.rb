1b26fa9dbf0d771a7c6d7f8c4a91dd4115132fd2
Abinoam Jr <abinoam@gmail.com>
2013-08-14 19:24:15 -0300

    Just fix the identation
    I think this was the last step that we can take
    without radically modifying the way the algorithm was trying to
    do for sorting.
    
    Next commits we are implementing some changes at the algorithm.

--- words_sort.rb
+++ words_sort.rb
@@ -6,21 +6,21 @@ words.pop
 more = 0
 
 while more < 1
-    0.upto(words.length-2) do |word_ix|
-        prev_word = words[word_ix]
-        next_word = words[word_ix+1]
-              if prev_word > next_word
-                words[word_ix] = next_word
-                words[word_ix+1] = prev_word
-                # reducing the more because we need an extra iteration
-                more -= 1
-                #Here, I skip over the rest of the each loop and do the
-                #while loop again
-                #so I don't over write any of the old values
-                break
-              end
+  0.upto(words.length-2) do |word_ix|
+    prev_word = words[word_ix]
+    next_word = words[word_ix+1]
+    if prev_word > next_word
+      words[word_ix] = next_word
+      words[word_ix+1] = prev_word
+      # reducing the more because we need an extra iteration
+      more -= 1
+      #Here, I skip over the rest of the each loop and do the
+      #while loop again
+      #so I don't over write any of the old values
+      break
     end
-    more += 1
+  end
+  more += 1
 end
 
 puts
