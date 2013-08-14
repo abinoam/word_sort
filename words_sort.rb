fdc0fcfcdc84478c79e421519b1507a0aaf6602f
Abinoam Jr <abinoam@gmail.com>
2013-08-14 11:29:21 -0300

    Better names for num1 and num2
    I'm trying to clearly understand the sorting algorithm.
    (Yes... I'm not good at them ;-) )

--- words_sort.rb
+++ words_sort.rb
@@ -3,26 +3,26 @@ words = []
 words.push gets.chomp while words.last != ''
 
 words.pop
-num1 = -1
-num2 = 0
+lower_ix = -1
+higher_ix = 0
 more = 0
 value1 = nil
 value2 = nil
 
 while more < words.length
     0.upto(words.length-1) do |word_ix|
-      if num1 < (words.length - 1)
+      if lower_ix < (words.length - 1)
         if value1 != 'reset'
           value2 = value1
           value1 = words[word_ix]
             if value2 and value1 and value1 != value2
               if value2 > value1
-                words[num1] = value1
-                words[num2] = value2
+                words[lower_ix] = value1
+                words[higher_ix] = value2
                 #Here, I skip over the rest of the each loop and do the
                 #while loop again
                 #so I don't over write any of the old values
-                num1 += words.length
+                lower_ix += words.length
                 # reducing the more because we need an extra iteration
                 more -= 1
               end
@@ -30,12 +30,12 @@ while more < words.length
         else
           value1 = words[word_ix]
         end
-          num1 += 1
-          num2 += 1
+          lower_ix += 1
+          higher_ix += 1
       end
     end
-    num1 = -1
-    num2 = 0
+    lower_ix = -1
+    higher_ix = 0
     value1 = 'reset'
     value2 = 'reset'
     more += 1
