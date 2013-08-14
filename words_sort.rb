8d923c5bd0b145f8d84123a0681967d171b87d1e
Abinoam Jr <abinoam@gmail.com>
2013-08-14 19:14:25 -0300

    Remove unecessary assignment and conditional with :reset and nil
    I think all the harassment of setting prev_word and next_word to nil
    or to :reset is related with trying to iterate from 0 indexed.
    
    I think we can just jump and get the first 2 elements and compare them.
    
    words[word_ix] is the first
    words[word_ix+1] is the second
    
    And at iteration they guard/maintain this relation.

--- words_sort.rb
+++ words_sort.rb
@@ -4,18 +4,15 @@ words.push gets.chomp while words.last != ''
 
 words.pop
 more = 0
-next_word = nil
-prev_word = nil
 
 while more < 1
-    0.upto(words.length-1) do |word_ix|
-        prev_word = next_word
-        next_word = words[word_ix]
-        if prev_word != :reset
+    0.upto(words.length-2) do |word_ix|
+        prev_word = words[word_ix]
+        next_word = words[word_ix+1]
             if prev_word and next_word and next_word != prev_word
               if prev_word > next_word
-                words[word_ix-1] = next_word
-                words[word_ix] = prev_word
+                words[word_ix] = next_word
+                words[word_ix+1] = prev_word
                 # reducing the more because we need an extra iteration
                 more -= 1
                 #Here, I skip over the rest of the each loop and do the
@@ -24,10 +21,7 @@ while more < 1
                 break
               end
             end
-        end
     end
-    next_word = :reset
-    prev_word = :reset
     more += 1
 end
 
