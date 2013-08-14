50f2dc5255fe5ee895695e68a12a5d5b6c901c90
Abinoam P. Marques Jr <abinoam@gmail.com>
2013-08-14 05:38:38 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+word_sort
+=========
+
+In reply to https://www.ruby-forum.com/topic/4416380
---
+++ words_sort.rb
@@ -0,0 +1,52 @@
+words = []
+endProg = nil
+
+while endProg != ''
+    words.push gets.chomp
+    endProg = words.last
+end
+
+words.pop
+num1 = -1
+num2 = 0
+more = 0
+value1 = nil
+value2 = nil
+
+while more < words.length
+    words.each do |sort|
+      if num1 < (words.length - 1)
+        if value1 != 'reset'
+          value2 = value1
+          value1 = sort
+            if value2 != nil and value1 != nil and value1 != value2
+              if value2 > value1
+                words[num1] = value1
+                words[num2] = value2
+                #Here, I skip over the rest of the each loop and do the
+                #while loop again
+                #so I don't over write any of the old values
+                num1 = num1 + words.length
+                # reducing the more because we need an extra iteration
+                more = more - 1
+              end
+            end
+        else
+          value1 = sort
+        end
+          num1 = num1 + 1
+          num2 = num2 + 1
+      end
+    end
+    words = words
+    num1 = -1
+    num2 = 0
+    sort = nil
+    value1 = 'reset'
+    value2 = 'reset'
+    more = more + 1
+end
+
+puts
+puts words
+puts
