743a44f3308106000d6130cd76418ee397b2d7c4
Abinoam Jr <abinoam@gmail.com>
2013-08-14 11:39:43 -0300

    Fix a bug with the 'reset'
    Making the word 'reset' the conditional introduces a bug.
    You'll never be able to sort correctly if the user enters 'reset' as one of the words.
    Changed it for Symbol (the user can't enter a Symbol at the keyboard)

--- words_sort.rb
+++ words_sort.rb
@@ -12,7 +12,7 @@ prev_word = nil
 while more < words.length
     0.upto(words.length-1) do |word_ix|
       if lower_ix < (words.length - 1)
-        if next_word != 'reset'
+        if next_word != :reset
           prev_word = next_word
           next_word = words[word_ix]
             if prev_word and next_word and next_word != prev_word
@@ -36,8 +36,8 @@ while more < words.length
     end
     lower_ix = -1
     higher_ix = 0
-    next_word = 'reset'
-    prev_word = 'reset'
+    next_word = :reset
+    prev_word = :reset
     more += 1
 end
 
