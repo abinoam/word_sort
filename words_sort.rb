b4856c4c1eea86cccb98f74866213e9bfdd8ec88
Abinoam Jr <abinoam@gmail.com>
2013-08-14 20:14:33 -0300

    Use parallel assignment for swapping words
    Parallel assignment of the type a,b=b,a make easier
    for us to get rid of the prev_word and next_word assignments.

--- words_sort.rb
+++ words_sort.rb
@@ -7,11 +7,8 @@ words.pop
 begin
   swapped = false
   0.upto(words.length-2) do |word_ix|
-    prev_word = words[word_ix]
-    next_word = words[word_ix+1]
-    if prev_word > next_word
-      words[word_ix] = next_word
-      words[word_ix+1] = prev_word
+    if words[word_ix] > words[word_ix+1]
+      words[word_ix], words[word_ix+1] = words[word_ix+1], words[word_ix]
       # reducing the more because we need an extra iteration
       swapped = true
     end
