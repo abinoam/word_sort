103e7b3f8e5fd978d1abe23dccccc0997c8f15c4
Abinoam Jr <abinoam@gmail.com>
2013-08-14 11:39:30 -0300

    Better names for value1 and value2

--- words_sort.rb
+++ words_sort.rb
@@ -6,19 +6,19 @@ words.pop
 lower_ix = -1
 higher_ix = 0
 more = 0
-value1 = nil
-value2 = nil
+next_word = nil
+prev_word = nil
 
 while more < words.length
     0.upto(words.length-1) do |word_ix|
       if lower_ix < (words.length - 1)
-        if value1 != 'reset'
-          value2 = value1
-          value1 = words[word_ix]
-            if value2 and value1 and value1 != value2
-              if value2 > value1
-                words[lower_ix] = value1
-                words[higher_ix] = value2
+        if next_word != 'reset'
+          prev_word = next_word
+          next_word = words[word_ix]
+            if prev_word and next_word and next_word != prev_word
+              if prev_word > next_word
+                words[lower_ix] = next_word
+                words[higher_ix] = prev_word
                 #Here, I skip over the rest of the each loop and do the
                 #while loop again
                 #so I don't over write any of the old values
@@ -28,7 +28,7 @@ while more < words.length
               end
             end
         else
-          value1 = words[word_ix]
+          next_word = words[word_ix]
         end
           lower_ix += 1
           higher_ix += 1
@@ -36,8 +36,8 @@ while more < words.length
     end
     lower_ix = -1
     higher_ix = 0
-    value1 = 'reset'
-    value2 = 'reset'
+    next_word = 'reset'
+    prev_word = 'reset'
     more += 1
 end
 
