46f605ecef98f01ed5f8cf009631714e82d57fbb
Abinoam Jr <abinoam@gmail.com>
2013-08-14 20:10:20 -0300

    Change 'more' to 'swapped': makes intentions clearer

--- words_sort.rb
+++ words_sort.rb
@@ -5,7 +5,7 @@ words.push gets.chomp while words.last != ''
 words.pop
 
 begin
-  more = false
+  swapped = false
   0.upto(words.length-2) do |word_ix|
     prev_word = words[word_ix]
     next_word = words[word_ix+1]
@@ -13,10 +13,10 @@ begin
       words[word_ix] = next_word
       words[word_ix+1] = prev_word
       # reducing the more because we need an extra iteration
-      more = true
+      swapped = true
     end
   end
-end while more
+end until not swapped
 
 puts
 puts words
